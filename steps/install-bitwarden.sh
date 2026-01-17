c5f8372b098af0f4fcd4f45b7605d2ca7c75c48d
Piotr Żabówka <wooboox@gmail.com>
2026-01-17 17:53:45 +0100

    fix bitwarden installation conflict by using npm for CLI

--- steps/install-bitwarden.sh
+++ steps/install-bitwarden.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-yay -S --noconfirm --needed bitwarden bitwarden-cli
-bw config server https://vault.bitwarden.eu
+yay -S --noconfirm --needed bitwarden
+
+if command -v npm >/dev/null 2>&1; then
+    npm install -g @bitwarden/cli
+fi
+
+if command -v bw >/dev/null 2>&1; then
+    bw config server https://vault.bitwarden.eu
+fi
